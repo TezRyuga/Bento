e1a1dc7b7b1c42c314f291a3dfa57ca307aec150
bhanu <bhanutezryuga@gmail.com>
2020-09-05 11:22:05 +0530

    minor changes to color and shadows

--- js/weather.js
+++ js/weather.js
@@ -13,7 +13,7 @@ var tempUnit = 'C';
 
 const KELVIN = 273.15;
 // Use your own key for the Weather, Get it here: https://openweathermap.org/
-const key = 'aa5b0a76dfbf87541928fb3cc32d3d69';
+const key = 'db38fb50a7282647e26c9c9b3fc7cf30';
 
 // Set Position function
 setPosition();
@@ -21,8 +21,8 @@ setPosition();
 function setPosition(position) {
     // Here you can change your position
     // You can use https://www.latlong.net/ to get it! (I use San Francisco as an example)
-    let latitude = 37.774929;
-    let longitude = -122.419418;
+    let latitude = 18.419670;
+    let longitude = 79.120300;
 
     getWeather(latitude, longitude);
 }
